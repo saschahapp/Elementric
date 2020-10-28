c3c02316db48ce746859239290711d8d9ec659ee
saschahapp <sascha.happ@leenmann.de>
2020-10-28 09:57:33 +0100

    new

--- src/view/dispatch.php
+++ src/view/dispatch.php
@@ -1,95 +1,106 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="view/CSS/stylesheet.css" />
-    <title>Newsletter-Versand</title>
-  </head>
-  <body>
-    <p style="text-align:right">
-      <?php if(!empty($statement)): ?>
-        <script>
-                alert("<?php echo filterHtmlTags($statement); ?>");
-        </script>
-      <?php endif; ?>
-    </p>
-    <form method="POST" action="logout">
-        <input style="text-align:right" type="submit" value="Logout" />
-    </form>
-    <div class="container">
-        <form enctype="multipart/form-data" action="dispatch" method="POST">
-            <p>
-                <strong>
-                    Laden Sie hier Ihre HTML-Datei hoch:
-                </strong>
-            </p>
-            <p>
-                <input name="htmlfile" type="file" accept="text/html" />
-                <input type="submit" value="Hochladen" />
-            </p>
-        </form>
-        <form enctype="multipart/form-data" action="dispatch" method="POST">
-            <p>
-                <strong>
-                    Laden Sie hier Ihre CSV-Datei hoch:
-                </strong>
-            </p>
-            <p>
-                <input name="csvfile" type="file" accept="text/csv" />
-                <input type="submit" value="Hochladen" />
-            </p>
-        </form>
-        <form style="margin-bottom:50px" method="POST" action="dispatch">
-            Wie viele templates wollen Sie hinzufügen? 
-            <input type="number" name="numbersOfTemplates">
-            <input type="submit" name="templateShow" value="Hinzufügen" />
-        </form>
-        <form method="POST" action="dispatch">
-            <?php if(!empty($_POST['numbersOfTemplates'])): ?>
-                <?php for ($i = 1; $i <= $_POST['numbersOfTemplates']; $i++): ?>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <title>Newsletter-Versand</title>
+    </head>
+    <body class="p-3 mb-2 bg-secondary text-white">
+        <div class="container">
+            <?php if(!empty($statement)): ?>
+                    <script>
+                            alert("<?php echo filterHtmlTags($statement); ?>");
+                    </script>
+            <?php endif; ?>
+            <div style="margin-bottom: 100px">
+                <form method="POST" style="float:left;margin-right: 50px" action="logout">
+                    <button type="submit"  class="btn btn-primary btn-lg">
+                        Logout  
+                    </button>
+                </form>
+                <form method="POST" style="float:left;margin-right: 50px" action="dispatch">
+                    <button type="submit" name="data" class="btn btn-primary btn-lg">Datei hochladen</button>
+                </form>
+                <form method="POST" action="dispatch">
+                    <button type="submit" name="dispatch" class="btn btn-primary btn-lg">Versand</button>
+                </form>
+            </div>
+            <?php if (isset($_POST['data'])): ?>
+                <form enctype="multipart/form-data" action="dispatch" method="POST">
                     <p>
-                        Wählen Sie Ihr Templates aus:
-                        <select style="margin-bottom:50px" name="templates<?php echo filterHtmlTags($i); ?>">
-                            <?php foreach($templates AS $template): ?>
-                                <option>
-                                    <?php echo filterHtmlTags($template) ?>
-                                </option>
-                            <?php endforeach; ?>
-                        </select>
+                        <strong>
+                            Laden Sie hier Ihre HTML-Datei hoch:
+                        </strong>
                     </p>
-                <?php endfor; ?>   
-            <?php endif;  ?>
-            <p>
-                Bitte geben Sie hier Ihre E-Mail ein (nur bei Testversand nötig!):
-            </p>
-            <p>
-                <input name="receiveremail" type="email" />
-            </p>
-            <p>
-                Bitte geben Sie hier Ihren Namen ein (nur bei Testversand nötig!):
-            </p>
-            <p>
-                <input name="receivername" type="text" />
-            </p>
-            <p> 
-            <p>
-                <input name="trialDispatch" type="checkbox" />
-                Test-Versand
-            </p>
-            <p>
-                <input name="customerDispatch" type="checkbox" />
-                KundenVersand
-            </p>
-            <p>
-                <input name="dispatch" type="submit" value="Bestätigen" />
-            </p>
-        </form>
-    </div>
+                    <p>
+                        <input name="htmlfile" type="file" accept="text/html" />
+                        <input type="submit" value="Hochladen" />
+                    </p>
+                </form>
+                <form enctype="multipart/form-data" action="dispatch" method="POST">
+                    <p>
+                        <strong>
+                            Laden Sie hier Ihre CSV-Datei hoch:
+                        </strong>
+                    </p>
+                    <p>
+                        <input name="csvfile" type="file" accept="text/csv" />
+                        <input type="submit" value="Hochladen" />
+                    </p>
+                </form>
+            <?php endif; ?>
+            <?php if(isset($_POST['dispatch']) || !empty($_POST['numbersOfTemplates'])): ?>
+                <form style="margin-bottom:50px" method="POST" action="dispatch">
+                    Wie viele templates wollen Sie hinzufügen? 
+                    <input type="number" name="numbersOfTemplates">
+                    <input type="submit" name="templateShow" value="Hinzufügen" />
+                </form>
+                <form method="POST" action="dispatch">
+                    <?php if(!empty($_POST['numbersOfTemplates'])): ?>
+                        <?php for ($i = 1; $i <= $_POST['numbersOfTemplates']; $i++): ?>
+                            <p>
+                                Wählen Sie Ihr Templates aus:
+                                <select style="margin-bottom:50px" name="templates<?php echo filterHtmlTags($i); ?>">
+                                    <?php foreach($templates AS $template): ?>
+                                        <option>
+                                            <?php echo filterHtmlTags($template) ?>
+                                        </option>
+                                    <?php endforeach; ?>
+                                </select>
+                            </p>
+                        <?php endfor; ?>   
+                    <?php endif;  ?>
+                    <p>
+                        Bitte geben Sie hier Ihre E-Mail ein (nur bei Testversand nötig!):
+                    </p>
+                    <p>
+                        <input name="receiveremail" type="email" />
+                    </p>
+                    <p>
+                        Bitte geben Sie hier Ihren Namen ein (nur bei Testversand nötig!):
+                    </p>
+                    <p>
+                        <input name="receivername" type="text" />
+                    </p>
+                    <p> 
+                    <p>
+                        <input name="trialDispatch" type="checkbox" />
+                        Test-Versand
+                    </p>
+                    <p>
+                        <input name="customerDispatch" type="checkbox" />
+                        KundenVersand
+                    </p>
+                    <p>
+                        <input name="dispatch" type="submit" value="Bestätigen" />
+                    </p>
+                </form>
+            <?php endif; ?>
+        </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
--- src/view/login.php
+++ src/view/login.php
@@ -7,32 +7,55 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="CSS/stylesheet.css" />
     <title>Login</title>
   </head>
-  <body>
+  <body class="p-3 mb-2 bg-secondary text-white">
     <div class="container">
-        <form method="POST" action="login">
+    <div class="row">
+        <div class="col-sm">
+        </div>
+        <div class="col-sm" style="margin-top: 200px">
+            <div style="margin-left: 100px">
+                <svg width="150px" height="150px" viewBox="0 0 16 16" class="bi bi-person-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
+                    <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                    <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
+                </svg>
+            </div>
+            <form method="POST" action="login">
+                <div class="input-group mb-3" style="margin-top: 30px">
+                    <div class="input-group-prepend ">
+                        <strong>
+                        <span class="input-group-text" id="inputGroup-sizing-default">Benutzername: </span>
+                        </strong>
+                        <input type="text" name="username" class="form-control " aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" required>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                            <strong>
+                            <span class="input-group-text" id="inputGroup-sizing-default">Passwort: </span>
+                            </strong>
+                            <input type="text" name="password" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" required>
+                        </div>
+                </div>
+                <div style="margin-left: 130px">
+                    <button type="submit" name="login-button" class="btn btn-primary btn-lg">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right-square-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm2.5 8.5a.5.5 0 0 1 0-1h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5z"/>
+                        </svg>
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
+        <div class="col-sm">
+        </div>
+    </div>
             <p>
-                <strong>
-                    Bitte geben Sie hier Ihren Benutzernamen ein:
-                </strong>
-                <input name="username" type="text" required />
-            </p>
-            <p>
-                <strong>
-                    Bitte geben Sie hier Ihr Passwort ein:
-                </strong>
-                <input name="password" type="text" required />
-            </p>
-            <p>
-                <input name="login-button" type="submit" value="LOGIN" />
-            </p>
-        </form>
-        <p>
             <?php if (isset($_POST['login-button'])): ?>
                 <strong>
-                    <?php echo $login; ?>
+                    <script> alert("<?php echo $login; ?>"); </script>
             </strong>
             <?php endif; ?>
         </p>
